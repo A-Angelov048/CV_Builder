5b32906545f803149753823aa563d740a49349be
Alexander Angelvo <alexander.angelov123@gmail.com>
2025-10-16 23:14:33 +0300

    Add comp to home cv page

--- client/src/pages/HomeCV.tsx
+++ client/src/pages/HomeCV.tsx
@@ -1,9 +1,25 @@
+import ContactMe from "../components/contactMe/ContactMe";
+import Footer from "../components/footer/Footer";
 import Header from "../components/header/Header";
+import ProfileCard from "../components/profileCard/ProfileCard";
+import Education from "../components/progress/Education";
+import Experience from "../components/progress/Experience";
+import Projects from "../components/projects/Projects";
+import Skills from "../components/skills/Skills";
+import SocialLinks from "../components/socialLinks/SocialLinks";
 
 export default function HomeCV() {
   return (
     <>
       <Header />
+      <ProfileCard />
+      <SocialLinks />
+      <Skills />
+      <Projects />
+      <Experience />
+      <Education />
+      <ContactMe />
+      <Footer />
     </>
   );
 }
