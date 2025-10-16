18f7c044638afd00574136c17cc8c2aaa37779c5
Alexander Angelvo <alexander.angelov123@gmail.com>
2025-10-16 21:43:31 +0300

    Add header comp

---
+++ client/src/components/header/Header.tsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+import styles from "./Header.module.css";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <div>
+        <Link className={styles.username} to="#">
+          Alex Angelov
+        </Link>
+      </div>
+      <nav className={styles.navbar}>
+        <input type={styles.checkbox} id={styles["menu-toggle"]} />
+        <label htmlFor="menu-toggle" className={styles["menu-icon"]}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </label>
+        <ul className={styles["nav-links"]}>
+          <li>
+            <Link to="#">Skills</Link>
+          </li>
+          <li>
+            <Link to="#">Projects</Link>
+          </li>
+          <li>
+            <Link to="#">Experience</Link>
+          </li>
+          <li>
+            <Link to="#">Education</Link>
+          </li>
+          <li>
+            <Link to="#">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
