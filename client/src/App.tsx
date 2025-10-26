99a90a2868ccdca9d7b2dd47fecf421ec2c63f2b
Alexander Angelvo <alexander.angelov123@gmail.com>
2025-10-26 16:54:20 +0200

    Update app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,45 @@
+import { Route, Routes } from "react-router-dom";
+
+import ScrollToTop from "./components/scrollToTop/ScrollToTop";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 import HomeCV from "./pages/HomeCV";
+import Register from "./pages/guestPages/Register";
+
+import { useScroll } from "./hooks/useScroll";
+import Login from "./pages/guestPages/Login";
+
+type Section =
+  | "about"
+  | "skills"
+  | "projects"
+  | "experience"
+  | "education"
+  | "contact";
 
 export default function App() {
+  const [sectionRefs, scrollToSection, scrollToUp] = useScroll<Section>([
+    "about",
+    "skills",
+    "projects",
+    "experience",
+    "education",
+    "contact",
+  ]);
+
   return (
     <>
-      <HomeCV />
+      <Header scrollFunc={scrollToSection} />
+
+      <ScrollToTop scrollUp={scrollToUp}>
+        <Routes>
+          <Route path="/" element={<HomeCV sectionRefs={sectionRefs} />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </ScrollToTop>
+
+      <Footer />
     </>
   );
 }
--- client/src/pages/HomeCV.tsx
+++ client/src/pages/HomeCV.tsx
@@ -1,25 +1,35 @@
 import ContactMe from "../components/contactMe/ContactMe";
-import Footer from "../components/footer/Footer";
-import Header from "../components/header/Header";
 import ProfileCard from "../components/profileCard/ProfileCard";
 import Education from "../components/progress/Education";
 import Experience from "../components/progress/Experience";
 import Projects from "../components/projects/Projects";
 import Skills from "../components/skills/Skills";
 import SocialLinks from "../components/socialLinks/SocialLinks";
+import RegisterNow from "../components/registerNow/RegisterNow";
 
-export default function HomeCV() {
+type Section =
+  | "about"
+  | "skills"
+  | "projects"
+  | "experience"
+  | "education"
+  | "contact";
+
+type HomeCVProps = {
+  sectionRefs: React.RefObject<Record<Section, HTMLDivElement | null>>;
+};
+
+export default function HomeCV({ sectionRefs }: HomeCVProps) {
   return (
     <>
-      <Header />
-      <ProfileCard />
+      <ProfileCard ref={(el) => void (sectionRefs.current.about = el)} />
       <SocialLinks />
-      <Skills />
-      <Projects />
-      <Experience />
-      <Education />
-      <ContactMe />
-      <Footer />
+      <Skills ref={(el) => void (sectionRefs.current.skills = el)} />
+      <Projects ref={(el) => void (sectionRefs.current.projects = el)} />
+      <Experience ref={(el) => void (sectionRefs.current.experience = el)} />
+      <Education ref={(el) => void (sectionRefs.current.education = el)} />
+      <ContactMe ref={(el) => void (sectionRefs.current.contact = el)} />
+      <RegisterNow />
     </>
   );
 }
