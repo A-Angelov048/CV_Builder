f754b9c61fa5959e0232734b60ff6fb34f534b22
Alexander Angelvo <153957490+A-Angelov048@users.noreply.github.com>
2025-12-11 22:59:35 +0200

    Add user model

---
+++ server/src/models/user.ts
@@ -0,0 +1,34 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IUser extends Document {
+  username: string;
+  email: string;
+  password: string;
+  refreshToken?: string;
+}
+
+const UserSchema = new Schema<IUser>(
+  {
+    username: {
+      type: String,
+      required: true,
+      minLength: [5, "Username should be at least 5 characters long."],
+    },
+    email: {
+      type: String,
+      required: true,
+      minLength: [10, "Email should be at least 10 characters long."],
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: [6, "Password should be at least 6 characters long."],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model<IUser>("User", UserSchema);
